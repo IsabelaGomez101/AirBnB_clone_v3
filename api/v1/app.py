083fb9814b6e6acb99f157f8e570f5bba2b5ca9b
CarolinaLopera <carolinalopera08@gmail.com>
2022-02-07 17:43:36 -0500

    Handle error 404

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Import package"""
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -16,6 +16,12 @@ port = getenv('HBNB_API_PORT') or 5000
 def teardown(exception):
     """Function to call storage.close"""
     storage.close()
+
+
+@app.errorhandler(404)
+def error_404(e):
+    """Handler for 404 errors, returns a JSON-formatted 404"""
+    return jsonify({"error": "Not found"})
 
 if __name__ == "__main__":
     """Main function"""
