ca10de61cb6c44f00233b1c58d1b42db4039e61f
IsabelaGomez101 <isa.gomc@gmail.com>
2022-02-07 11:30:13 -0500

    adding tests

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -113,3 +113,8 @@ class TestFileStorage(unittest.TestCase):
         with open("file.json", "r") as f:
             js = f.read()
         self.assertEqual(json.loads(string), json.loads(js))
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_get(self):
+        """test method get"""
+        self.assertIs(type(models.storage.get()), dict)
